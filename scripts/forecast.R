9b5c438e24443b1945196da0c4092fc384ff7b83
Joe Hunter <hunter.usmc@gmail.com>
2020-08-17 11:19:19 -0500

    - updated forecast script with manual arima and prophet demos

--- scripts/forecast.R
+++ scripts/forecast.R
@@ -1,18 +1,40 @@
 #install.packages('tidyverse')
 #install.packages('forecast')
+#install.packages('prophet')
+#data(package = .packages(all.available = T))
 
 library('tidyverse')
 library('forecast')
+options(scipen = 999, stringsAsFactors = F)
 
-forecast::gas
-forecast::gold
-forecast::taylor
-forecast::wineind
-forecast::woolyrnq
+##### auto.arima #####
+plot(co2)
 
-plot(forecast::gas)
+autoarima_fit <- auto.arima(co2)
+autoarima_forecast <- forecast(autoarima_fit, h = 60)
 
-gas_fit_1 <- auto.arima(forecast::gas)
-gas_forecast_1 <- forecast(gas_fit_1, h = 60)
+plot(autoarima_forecast)
 
-plot(gas_forecast_1)
+##### arima #####
+acf(co2)
+pacf(co2)
+ar(co2)
+
+arima_fit <- arima(co2, order = c(1L, 1L, 1L),
+                   seasonal = list(order = c(1L, 1L, 1L), period = 12))
+arima_forecast <- forecast(arima_fit, h = 60)
+
+plot(arima_forecast)
+
+##### prophet #####
+library('prophet')
+
+data <- read_csv('https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-total-female-births.csv')
+
+plot(data, type = 'l')
+
+prophet_model <- prophet(yearly.seasonality = T, daily.seasonality = F,
+                         select(data, ds = Date, y = Births))
+#fit.prophet(prophet_model)
+prophet_forecast <- predict(prophet_model, make_future_dataframe(prophet_model, periods = 50, freq = 'day'))
+plot(prophet_model, prophet_forecast)
